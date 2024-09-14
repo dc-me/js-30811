0aa9fe3b6231e8d57c999816d7fbb14da191dd14
dany <danychen.me@gmail.com>
2024-09-13 22:10:10 -0700

    add part 2 prime time

--- js30831.js
+++ js30831.js
@@ -1,6 +1,6 @@
 // ================================================================
 // Part 1: Fizz Buzz
-
+console.log('=================== Fizz Buzz ====================');
 // Loop through all numbers from 1 to 100.
 // If a number is divisible by 3, log “Fizz.”
 // If a number is divisible by 5, log “Buzz.”
@@ -22,5 +22,29 @@ for (let i = 1; i <= 100; i++) {
   }
   if (!(isFizz || isBuzz)) {
     console.log(`Number: ${i} Neither Fizz or Buzz.`);
+  }
+}
+
+// ================================================================
+// Part 2: Prime Time
+console.log('=================== Prime Time ====================');
+// Declare an arbitrary number, n.
+// Create a loop that searches for the next prime number, starting at n and incrementing from there.
+// As soon as you find the prime number, log that number and exit the loop.
+let n = 50;
+let lookingPrime = true;
+n = n < 1 ? 2 : n; //1 and negative are not prime numbers.
+while (lookingPrime) {
+  let isPrime = true;
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      n++;
+      isPrime = false;
+      break;
+    }
+  }
+  if (isPrime) {
+    lookingPrime = false;
+    console.log(`Found prime number: ${n}`);
   }
 }
